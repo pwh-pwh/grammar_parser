6b3f3ee1ae622f9b1a280e500b7f4bf3e9aea35d
pwh-pwh <plmoknpwh@163.com>
2021-11-18 00:08:18 +0800

    完成基本ui的设计

--- src/main/java/com/example/grammar_parser/controller/HelloController.java
+++ src/main/java/com/example/grammar_parser/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.grammar_parser;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button openFileBtn;
 
+    @FXML
+    protected void onOpenFileBtnClick() {
+
+    }
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
---
+++ src/main/java/com/example/grammar_parser/service/GrammarService.java
@@ -0,0 +1,2 @@
+package com.example.grammar_parser.service;public class GrammarService {
+}
